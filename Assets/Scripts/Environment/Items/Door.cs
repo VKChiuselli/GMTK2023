2d1019556598620305e9a63b393f8a63ed9e9ca7
VKChiuselli <vittorio.chiuselli@gmail.com>
2023-07-08 21:17:09 +0200

    Implement door

--- Assets/Scripts/Environment/Items/Door.cs
+++ Assets/Scripts/Environment/Items/Door.cs
@@ -4,5 +4,45 @@ using UnityEngine;
 
 public class Door : InteractItem
 {
-  
+    bool isOpen;
+    public List<Sprite> openCloseSprites;
+
+    private void Start()
+    {
+        isOpen = false;
+    }
+
+    private void Update()
+    {
+     
+        if (isOpen)
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = openCloseSprites[0];
+        }
+        else
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = openCloseSprites[1];
+        }
+    }
+
+    public void OpenDoor()
+    {
+        isOpen = true;
+    }
+
+    public void CloseDoor()
+    {
+        isOpen = false;
+    }
+
+    public void PlayOpenDoorSFX()
+    {
+        GetComponent<SFX>().PlayFirstEffect();
+    } 
+
+    public void ChangeFloor()
+    {
+        Debug.Log("TODO");
+    }
+
 }
