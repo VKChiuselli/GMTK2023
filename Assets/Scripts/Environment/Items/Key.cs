8ec438e7b5c67f5a0766311da2e9f4dddb843dd5
VKChiuselli <vittorio.chiuselli@gmail.com>
2023-07-08 21:54:54 +0200

    Implement prefab door and key

--- Assets/Scripts/Environment/Items/Key.cs
+++ Assets/Scripts/Environment/Items/Key.cs
@@ -1,8 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Key : InteractItem
 {
-  
+    bool isPickedUp;
+    Door door;
+
+    private void Start()
+    {
+        isPickedUp = false;
+        door = FindObjectOfType<Door>();
+    }
+
+   
+
+    public override void InteractWith(Unit unit)
+    {
+        isPickedUp = true;
+        OpenDoor();
+        PlayPickKeySFX();
+    }
+
+    public void OpenDoor()
+    {
+        door.OpenDoor();
+    }
+    public void PlayPickKeySFX()
+    {
+        GetComponent<SFX>().PlayFirstEffect();
+    }
+
 }
