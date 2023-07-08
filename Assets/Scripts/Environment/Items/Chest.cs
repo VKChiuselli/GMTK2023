6bf9ed25619030114fbb73c3aa021c14108f59d4
VKChiuselli <vittorio.chiuselli@gmail.com>
2023-07-08 21:04:16 +0200

    Implement chest script

--- Assets/Scripts/Environment/Items/Chest.cs
+++ Assets/Scripts/Environment/Items/Chest.cs
@@ -5,23 +5,59 @@ using UnityEngine;
 public class Chest : InteractItem
 {
     bool isOpen;
+    bool isEmpty;
     public List<Sprite> openCloseSprites;
+    public GameObject itemContained;
 
     private void Start()
     {
+        itemContained = null;
         isOpen = false;
+        isEmpty = true;
     }
 
     private void Update()
     {
+        //if (itemContained != null)
+        //{
+        //    isEmpty = false;
+        //}
+        //else
+        //{
+        //    isEmpty = true;
+        //}
+
         if (isOpen)
         {
-           transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = openCloseSprites[0];
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = openCloseSprites[0];
         }
         else
         {
             transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = openCloseSprites[1];
         }
+    }
+
+    public void InsertItem(GameObject insertItem)
+    {
+        itemContained = insertItem;
+        isEmpty = false;
+    }
+
+    public GameObject LootItem()
+    {
+        isEmpty = true;
+        return itemContained;
+
+    }
+
+    public void OpenChest()
+    {
+        isOpen = true;
+    }
+
+    public void CloseChest()
+    {
+        isOpen = false;
     }
 
     public void PlayOpenChestSFX()
