3d848380efb13416f6911d2676f64da67d80e248
VKChiuselli <vittorio.chiuselli@gmail.com>
2023-07-08 20:02:00 +0200

    Implement hero sfx

--- Assets/Scripts/Player/Hero.cs
+++ Assets/Scripts/Player/Hero.cs
@@ -4,4 +4,31 @@ using UnityEngine;
 
 public class Hero : Unit
 {
+    public void PlayArrowSFX()
+    {
+        GetComponent<SFX>().PlayOneSpecific(0);
+    } 
+
+    public void PlayAttackSFX()
+    {
+        GetComponent<SFX>().PlayOneSpecific(1);
+    } 
+
+    public void PlayShieldBlockSFX()
+    {
+        GetComponent<SFX>().PlayOneSpecific(2);
+    } 
+
+    public void PlayTakingDamageSFX()
+    {
+        GetComponent<SFX>().PlayOneSpecific(3);
+    } 
+
+    public void PlayTorchSFX()
+    {
+        GetComponent<SFX>().PlayOneSpecific(4);
+    } 
+
+
+
 }
