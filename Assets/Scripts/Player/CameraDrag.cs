a336bcb83b4c7bcf5c8e0ebd166c2c9500ec5593
VKChiuselli <vittorio.chiuselli@gmail.com>
2023-07-08 15:52:42 +0200

    Make limits to move the camera

--- Assets/Scripts/Player/CameraDrag.cs
+++ Assets/Scripts/Player/CameraDrag.cs
@@ -28,8 +28,29 @@ public class CameraDrag : MonoBehaviour
     {
         if (!_isDragging) return;
 
-        _difference = GetMousePosition - transform.position;
+        _difference = GetMousePosition - transform.position; 
         transform.position = _origin - _difference;
+        if (transform.position.y< -3)
+        {
+            float x  = transform.position.x;
+            transform.position = new Vector3(x, -3,-10 );
+        }
+        if (transform.position.y> +3)
+        {
+            float x  = transform.position.x;
+            transform.position = new Vector3(x,  3,-10 );
+        }
+
+        if (transform.position.x< -5)
+        {
+            float y  = transform.position.y;
+            transform.position = new Vector3(-5, y,-10 );
+        }
+        if (transform.position.x> +3)
+        {
+            float y  = transform.position.y;
+            transform.position = new Vector3(5,  y,-10 );
+        }
     }
 
     private Vector3 GetMousePosition => _mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
