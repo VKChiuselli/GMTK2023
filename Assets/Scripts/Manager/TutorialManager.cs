f5d99062e57a1e88b0a0f1e5764a9c4f699cabee
VKChiuselli <vittorio.chiuselli@gmail.com>
2023-07-09 08:03:11 +0200

    Change chest sprite

---
+++ Assets/Scripts/Manager/TutorialManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    bool isTutorialProgress;
+
+    void Update()
+    {
+        if (isTutorialProgress)
+        {
+        
+        }
+    }
+
+    private void StartTutorial()
+    {
+        Debug.Log("TODO");
+    }
+}
