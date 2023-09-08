79c46f8471b37431ec0c12dffcc7c0c3a4313a59
VKChiuselli <vittorio.chiuselli@gmail.com>
2023-09-08 18:22:41 +0200

    Setup isometric scene from tutorial

---
+++ Assets/PathfindingModule/Finished/Part3/Scripts/CharacterInfo.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace finished3
+{
+    public class CharacterInfo : MonoBehaviour
+    {
+        public OverlayTile standingOnTile;
+
+
+    //    private void Update()
+    //    {
+    //        GetComponent<SpriteRenderer>().sortingOrder = 99;
+    //}
+
+    }
+
+  
+}
